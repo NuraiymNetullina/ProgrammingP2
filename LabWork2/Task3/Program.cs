7c59989a8326aec025982bfbf52d2b4118fd4d5f
NuraiymNetullina <47219259+NuraiymNetullina@users.noreply.github.com>
2019-02-16 04:30:27 +0600

    Update Program.cs

--- LabWork2/Task3/Program.cs
+++ LabWork2/Task3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,29 +11,29 @@ namespace Task3
     {
         static void Line(int line)
         {
-            for(int i=0; i < line; ++i)
+            for(int i=0; i < line; ++i) // пробегаемся через цикл фор по строке
             {
-                Console.Write("   ");
+                Console.Write("   "); // при увеличении индекса добавляем пустой отрезок 
             }
         }
         static void File(DirectoryInfo dir, int line)
         {
             foreach (FileInfo f in dir.GetFiles())
             {
-                Line(line);
-                Console.WriteLine(f.Name);
+                Line(line); // отправляем в функцию лайн
+                Console.WriteLine(f.Name); // выводим имя файла
             }
             foreach (DirectoryInfo d in dir.GetDirectories())
             {
-                Line(line);
-                Console.WriteLine(d.Name);
-                File(d, line + 1);
+                Line(line); // отправляем в функцию лайн
+                Console.WriteLine(d.Name); // выводим имя папки
+                File(d, line + 1); 
             }
         }
         static void Main(string[] args)
         {
             DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Nurayim\Desktop\KBTU\Semester-2"); // ссылка на папку
-            File(dir, 0);
+            File(dir, 0); // отправляем в функцию файл
             Console.ReadKey();
            
         }
