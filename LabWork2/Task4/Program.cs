326600cf187fd02bfc765f76833f30e6cad8ea52
NuraiymNetullina <47219259+NuraiymNetullina@users.noreply.github.com>
2019-02-16 04:31:49 +0600

    Update Program.cs

--- LabWork2/Task4/Program.cs
+++ LabWork2/Task4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +11,17 @@ namespace Task4
     {
         static void Main(string[] args)
         {
+            string s = "Hello Woeld";
+            StreamWriter file = new StreamWriter(@"C:\Users\Nurayim\Desktop\qwerty\zxcvb\Test.txt"); // ссылка папки, где создаем новый файл 
+            string path2 = @"C:\Users\Nurayim\Desktop\qwerty\output.txt"; // ссылка файла, куда переносим содержимое строки  
+            file.Write(s); // функция, которая присваивает значение
+            file.Close(); // закрываем файл
+            string path = @"C:\Users\Nurayim\Desktop\qwerty\zxcvb\Test.txt"; // ссылка файла, где хранится строка
+            if (File.Exists(path2)) //Проверяем есть ли существующий файл
+            {
+                File.Delete(path2); //Если есть, то удаляем.
+                File.Move(path, path2); //через функцию мув, перемещаем файл с path в path2
+            }
         }
     }
 }
